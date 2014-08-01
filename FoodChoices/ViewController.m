f96526f4db1cd6411e0179dad03454065dea8149
Kevin Moy <kevintmoy@Kevins-MacBook-Air-2.local>
2014-07-31 23:21:01 -0700

    Add search bar and table cell

--- FoodChoices/ViewController.m
+++ FoodChoices/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property(nonatomic, strong) NSArray *datasourceArray;
+@property(nonatomic, strong) NSArray *searchResults;
+//    NSArray *countries;
 
 @end
 
@@ -17,7 +21,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.datasourceArray = [[NSArray alloc] initWithObjects:@"Chinese", @"Japanese", @"Korean", @"Vietnamese", @"Mexican", nil];
+    self.searchResults = [[NSArray alloc] init];
+    
+	// Do  any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +33,46 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView == self.searchDisplayController.searchResultsTableView
+        )
+    {
+        return [self.searchResults count];
+    }
+    else
+    {
+        return self.datasourceArray.count;
+    }
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellID = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    if (tableView == self.searchDisplayController.searchResultsTableView)
+    {
+        cell.textLabel.text = [self.searchResults objectAtIndex:indexPath.row];
+    }
+    else
+    {
+    cell.textLabel.text = [self.datasourceArray objectAtIndex:indexPath.row];
+    }
+    return cell;
+    
+}
+- (void)filterContentForSearchText:(NSString *)searchText scope:(NSString *)scope
+{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF beginswith[c] %@", searchText];
+    self.searchResults = [self.datasourceArray filteredArrayUsingPredicate:predicate];
+}
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString scope:[[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
+    return YES;
+}
 @end
+
